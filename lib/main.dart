bc7dab4b8a9110c5d5a54f8e42bdf094a936d77a
Xaled Fetah <tyyutshis@gmail.com>
2023-04-13 23:41:10 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_s1/class_horizental_card.dart';
 import 'package:flutter_application_s1/home_page.dart';
-import 'package:flutter_application_s1/page13_categories.dart';
-import 'package:flutter_application_s1/page14_catigoreis_sub_details.dart';
+import 'package:flutter_application_s1/page_30_natificattion.dart';
+import 'package:flutter_application_s1/page_13_categories.dart';
+import 'package:flutter_application_s1/page_14_catigoreis_sub_details.dart';
 import 'package:flutter_application_s1/page_9.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
@@ -20,16 +21,21 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        appBarTheme:
+            AppBarTheme(color: Colors.white, surfaceTintColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.white,
+        ),
         useMaterial3: true,
       ),
       routes: {
         "home": (context) => Home_p(),
         "page_9": (context) => Page_9(),
         "page_13": (context) => Page_13(),
+        "page_30": (context) => Page_30(),
         "page_14": (context) => CatSubDet(),
       },
-      home: Home_p(),
+      home: Page_30(),
     );
   }
 }
